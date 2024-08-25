fd3862415d1c813a4d54194521bb16e73a362dc7
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-25 13:35:46 +0530

    Create alert_send.py

---
+++ alert_send.py
@@ -0,0 +1,8 @@
+def send_to_email(breachType):
+  recepient = "a.b@c.com"
+  if breachType == 'TOO_LOW':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too low')
+  elif breachType == 'TOO_HIGH':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too high')
