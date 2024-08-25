e81d14f63bf76faefb7f70bd95f4027da06f3ed4
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-25 13:16:43 +0530

    Create check_alert.py

---
+++ check_alert.py
@@ -0,0 +1,9 @@
+def check_and_alert(alertTarget, batteryChar, temperatureInC):
+    breachType = classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+    
+    if alertTarget == 'TO_CONTROLLER':
+        send_to_controller(breachType)
+    elif alertTarget == 'TO_EMAIL':
+        send_to_email(breachType)
+    else:
+        raise ValueError(f"Unknown alert target: {alertTarget}")
